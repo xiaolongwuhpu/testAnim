8e5b5f9a194e0f5010fa127a0c78c1bd883d5b93
wujing <wujing@nutfin.cn>
2018-05-29 20:04:20 +0800

    rename

--- app/src/main/java/com/testanim/longwu/activity/MainActivity.java
+++ app/src/main/java/com/testanim/longwu/activity/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_1);
         ButterKnife.bind(this);
     }
 
